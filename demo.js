8950a8a14823e888e7bf96ba25e405df7f457938
Douglas <douglas@mediushealth.org>
2021-01-29 10:28:56 +1100

    test 1

--- demo.js
+++ demo.js
@@ -23,22 +23,17 @@ function checkForRelatedApps() {
     console.log("Android");
     window.location.href =
       "https://play.google.com/store/apps/details?id=au.com.medinet.mypractice";
-  } else if (window.navigator.userAgent.includes("iPhone")) {
+  } else if (
+    navigator.userAgent.includes("iPhone") ||
+    navigator.userAgent.includes("iPad")
+  ) {
     window.location.href =
-      "https://apps.apple.com/au/app/mypractice/id1542728412";
-  } else if (window.navigator.userAgent.includes("Safari")) {
-    console.log("Safari");
-    window.location.href =
-      "https://apps.apple.com/au/app/mypractice/id1542728412";
-  } else if (navigator.userAgent.includes("iPhone")) {
-    document.location.href =
       "https://apps.apple.com/au/app/mypractice/id1542728412";
   } else if (navigator.userAgent.includes("Safari")) {
     console.log("Safari");
     window.location.href =
       "https://apps.apple.com/au/app/mypractice/id1542728412";
   }
-
   console.log(
     "🚀 ~ file: demo.js ~ line 15 ~ navigator.getInstalledRelatedApps ~ relatedApps",
     relatedApps
