c99a806f6cbf7b1e3d91fd735593d873e44c0437
Douglas <douglas@mediushealth.org>
2021-01-29 11:23:12 +1100

    test 1

--- demo.js
+++ demo.js
@@ -5,19 +5,17 @@ window.addEventListener("load", () => {
     console.log("Android");
     window.location.href =
       "https://play.google.com/store/apps/details?id=au.com.medinet.mypractice";
-  } else if (window.navigator.userAgent.includes("iPhone")) {
+  } else if (navigator.userAgent.includes("iPhone")) {
     window.location.href =
       "https://apps.apple.com/au/app/mypractice/id1542728412";
-  } else if (window.navigator.userAgent.includes("Safari")) {
-    console.log("Safari");
+  } else if (navigator.userAgent.includes("Safari")) {
     window.location.href =
       "https://apps.apple.com/au/app/mypractice/id1542728412";
   } else if (navigator.userAgent.includes("iPhone")) {
     document.location.href =
       "https://apps.apple.com/au/app/mypractice/id1542728412";
-  } else if (navigator.userAgent.includes("Safari")) {
-    console.log("Safari");
+  } else if (navigator.userAgent.includes("Chrome")) {
     window.location.href =
-      "https://apps.apple.com/au/app/mypractice/id1542728412";
+      "https://play.google.com/store/apps/details?id=au.com.medinet.mypractice";
   }
 });
