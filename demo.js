dda1682a60d102d5a1f9c25d37ef33d458d61fe1
Douglas <douglas@mediushealth.org>
2021-03-07 12:00:38 +1100

    change DL URL

--- demo.js
+++ demo.js
@@ -4,21 +4,21 @@ window.addEventListener("load", () => {
   if (navigator.userAgent.includes("Android")) {
     console.log("Android");
     window.location.href =
-      "https://play.google.com/store/apps/details?id=au.com.medinet.mypractice";
+      "https://play.google.com/store/apps/details?id=com.google.android.youtube";
   } else if (
     navigator.userAgent.includes("iPhone") ||
     navigator.userAgent.includes("iPad")
   ) {
     window.location.href =
-      "https://apps.apple.com/au/app/mypractice/id1542728412";
+      "https://apps.apple.com/au/app/youtube-watch-listen-stream/id544007664";
   } else if (navigator.userAgent.includes("Chrome")) {
     window.location.href =
-      "https://play.google.com/store/apps/details?id=au.com.medinet.mypractice";
+      "https://play.google.com/store/apps/details?id=com.google.android.youtube";
   } else if (navigator.userAgent.includes("Safari")) {
     document.location.href =
-      "https://apps.apple.com/au/app/mypractice/id1542728412";
+      "https://apps.apple.com/au/app/youtube-watch-listen-stream/id544007664";
   } else if (navigator.userAgent.includes("Firefox")) {
     window.location.href =
-      "https://play.google.com/store/apps/details?id=au.com.medinet.mypractice";
+      "https://play.google.com/store/apps/details?id=com.google.android.youtube";
   }
 });
